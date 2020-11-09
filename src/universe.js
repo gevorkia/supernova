38e30f495c35032e7d49f0162e3f6d9c8281e3cb
gevorkia <liligevorkian@gmail.com>
2020-11-08 20:32:09 -0800

    Add modal functionality

--- src/universe.js
+++ src/universe.js
@@ -28,6 +28,7 @@ class Universe {
     new Star(this.scene, star00000);
 
     this.eventListeners();
+    this.modal();
 
     this.starFiles = [
       star00000, 
@@ -67,6 +68,35 @@ class Universe {
     };
 
     animate();
+  }
+
+  modal() {
+    // Get the modal
+    console.log("modal fxn")
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("about-btn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+      modal.style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
   }
 
   eventListeners() {
